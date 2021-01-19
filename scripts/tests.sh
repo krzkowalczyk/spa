a10f8064ce5502a4291777a27606ae79e0db4cfb
Piotr Kojałowicz <Piotr.Kojalowicz@tomtom.com>
2021-01-19 18:45:04 +0100

    working test

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -12,10 +12,10 @@ function build_test () {
 
 function container_test () {
   docker run -it --name $project_name -d -p $docker_port_host:$docker_port_guest $registry_url:$registry_port/$project_name:$project_version
-  if curl -I "https://localhost:8080" 2>&1 | grep -w "200\|301" ; then
-    echo "docker is up"
+  if curl -I localhost:8080 2>&1 | grep -w "200\|301" ; then
+    echo "Test it docker is up - OK, good job :D!"
   else
-    echo "docker fail"
+    echo "Test it docker is up - docker fail :("
     exit 1
   fi
   docker stop $project_name || true && docker rm $project_name || true
